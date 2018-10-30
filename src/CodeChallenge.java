c62502c01e7e9f2ef50256b7f7a2e1c63ad2bc06
zzSandyzz <tendersymphony@gmail.com>
2018-10-31 00:36:59 +0300

    The next message

--- src/CodeChallenge.java
+++ src/CodeChallenge.java
@@ -2,8 +2,7 @@ public class CodeChallenge {
 
 	public static void main(String[] args) {
 		System.out.println("111");
-		//Okay, the initial plan was to get shit done today, but it's past midnight,
-		// who am i kidding
+		//I really want a tshit 
 
 	}
 
