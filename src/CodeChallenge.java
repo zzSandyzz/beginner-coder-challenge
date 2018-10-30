29fe222446fb45a4efa2007a836d021970c84ced
zzSandyzz <tendersymphony@gmail.com>
2018-10-31 00:44:25 +0300

    skjdfjs

--- src/CodeChallenge.java
+++ src/CodeChallenge.java
@@ -1,8 +1,8 @@
 public class CodeChallenge {
 
 	public static void main(String[] args) {
-		System.out.println("222");
-		//I really want a tshit 
+		System.out.println("2");
+		//I really want a tshrit 
 		
 	    //I want us both to have tshirts
 
