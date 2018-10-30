068217270dd5b2fa227559992819b9c65167c914
zzSandyzz <tendersymphony@gmail.com>
2018-10-31 00:26:38 +0300

    The first heartfelt message comming through

--- src/CodeChallenge.java
+++ src/CodeChallenge.java
@@ -1,7 +1,9 @@
 public class CodeChallenge {
 
 	public static void main(String[] args) {
-		System.out.println("222");
+		System.out.println("111");
+		//Okay, the initial plan was to get shit done today, but it's past midnight,
+		// who am i kidding
 
 	}
 
